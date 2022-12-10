0033622c6ed7930fdf92d9989f2ffee05e130ee7
KurbatovIOS <akurbatov.work@gmail.com>
2022-12-10 21:43:09 +0300

    Initial Commit

---
+++ Store/StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreApp.swift
+//  Store
+//
+//  Created by Kurbatov Artem on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
